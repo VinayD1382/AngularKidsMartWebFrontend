debd196237a1b71f66f9d080b2e612b00fa1b545
Vinay D <114236808+VinayD1382@users.noreply.github.com>
2025-09-27 01:59:19 +0530

    Update Electronic.service.ts

--- src/app/services/Electronic.service.ts
+++ src/app/services/Electronic.service.ts
@@ -7,7 +7,7 @@ import { ProductItem } from '../models/product.model';
   providedIn: 'root'
 })
 export class ElectronicProductService {
-  private apiUrl = 'http://localhost:5000/api/angular-kidsele';
+  private apiUrl = 'https://angularkidsmartwebbackend.onrender.com/api/angular-kidsele';
 
   constructor(private http: HttpClient) {}
 
