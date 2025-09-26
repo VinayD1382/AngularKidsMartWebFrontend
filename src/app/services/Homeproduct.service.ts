3d5dbe98709e3011a7a74d210d7a1d2387201b9b
Vinay D <114236808+VinayD1382@users.noreply.github.com>
2025-09-27 02:00:14 +0530

    Update Homeproduct.service.ts

--- src/app/services/Homeproduct.service.ts
+++ src/app/services/Homeproduct.service.ts
@@ -7,7 +7,7 @@ import { ProductItem } from '../models/product.model';
   providedIn: 'root'
 })
 export class HomeProductService {
-  private apiUrl = 'http://localhost:5000/api/angular-home-products';
+  private apiUrl = 'https://angularkidsmartwebbackend.onrender.com/api/angular-home-products';
 
   constructor(private http: HttpClient) {}
 
